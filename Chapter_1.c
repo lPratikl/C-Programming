8d4ef12e5f480d2fecec68d24acb5920f638d237
lPratikl <142092240+lPratikl@users.noreply.github.com>
2023-08-12 19:19:43 +0800

    Add files via upload

---
+++ Chapter_1.c
@@ -0,0 +1,52 @@
+# include<stdio.h>
+
+int main() {
+ //....................................................................................
+ // Simple math of 2 inputs 
+    // int a, b;
+    // printf("enter a ");
+    // scanf("%d", &a);
+
+    // printf("enter b ");
+    // scanf("%d", &b);
+
+    // printf("sum is: %d \n", a + b);
+    // printf("diff is: %d \n", a - b);
+    // printf("prod is: %d \n", a*b);
+    // printf("div is: %d \n", a/b);
+
+//....................................................................................
+// Program to calculate area of a square 
+    // int side;
+    // printf("enter lenght of a side: ");
+    // scanf("%d", &side);
+    // printf("Area of square = %d \n", side*side);
+
+    // float side2;
+    // printf("enter float lenght of a side: ");
+    // scanf("%f", &side2);
+    // printf("Area of square = %f \n", side2*side2);
+
+//.......................................................................................
+// Program to calculate area of a circle 
+    // float radius;
+    // printf("Enter the radius: ");
+    // scanf("%f", &radius);
+    // printf("Area of circle = %f", 3.14*radius* radius);
+
+//........................................................................................
+// Homework 1: Calculate teh perimeter of a rectangle
+    int a, b, n;
+    printf("Enter sides a: "); 
+    scanf("%d", &a);
+    printf("Enter sides b: "); 
+    scanf("%d", &b);
+    printf("Perimeter of rectangle = %d \n", 2*(a+b));
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Cube of %d = %d", n, n*n*n);
+
+
+    return 0;
+}
